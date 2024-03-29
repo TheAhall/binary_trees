59378383b193d31bc929801eae9c8bc3fd73a1c9
HALLAL <hmdhallal@gmail.com>
2024-03-28 19:48:12 -0700

    0x1D. C - Binary trees

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -8,13 +8,13 @@
  */
 void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level)
 {
-        if (level == 1)
-                func(tree->n);
-        else
-        {
-                btlo_helper(tree->left, func, level - 1);
-                btlo_helper(tree->right, func, level - 1);
-        }
+	if (level == 1)
+		func(tree->n);
+	else
+	{
+		btlo_helper(tree->left, func, level - 1);
+		btlo_helper(tree->right, func, level - 1);
+	}
 }
 
 /**
@@ -26,15 +26,15 @@ void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t height_l = 0;
-        size_t height_r = 0;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-        if (!tree)
-                return (0);
+	if (!tree)
+		return (0);
 
-        height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-        height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-        return (height_l > height_r ? height_l : height_r);
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
 
 /**
